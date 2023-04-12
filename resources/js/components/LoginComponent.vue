8069faa458994ffc8204666c8bdcadb81cef7389
nymeria-1 <mjflagonoy@tip.edu.ph>
2023-04-12 17:15:38 +0800

    Login view and route

---
+++ resources/js/components/LoginComponent.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Login</div>
+    
+                    <div class="card-body">
+                        <form action="/account/login" method="post">
+                            <input type="hidden" name="_token" :value="csrf">
+                            <div class="row mb-3">
+                                <label for="username" class="col-md-4 col-form-label text-md-end">Username</label>
+    
+                                <div class="col-md-6">
+                                    <input id="username" type="username" class="form-control" name="username" v-model="fields.username" required autocomplete="username" autofocus>
+                                    <span v-if="errors && errors.username" class="invalid-feedback" role="alert">
+                                        <strong>{{ errors.username[0] }}</strong>
+                                    </span>
+                                </div>
+                            </div>
+    
+                            <div class="row mb-3">
+                                <label for="password" class="col-md-4 col-form-label text-md-end">Password</label>
+    
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" name="password" v-model="fields.password" required>
+                                    <span v-if="errors && errors.password" class="invalid-feedback" role="alert">
+                                        <strong>{{ errors.password[0] }}</strong>
+                                    </span>
+                                </div>
+                            </div>
+    
+                            <div class="row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Login
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </template>
+    
+<script>
+    export default {
+      data() {
+        return {
+          fields: {},
+          errors: {},
+          csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+        }
+      },
+      methods: {
+    
+      },
+    }
+</script>
+    
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'Welcome!';
 });
+
+Route::get('/account/login', [AuthController::class, 'index'])->name('login');
+Route::post('/account/login', [AuthController::class, 'signin']);
