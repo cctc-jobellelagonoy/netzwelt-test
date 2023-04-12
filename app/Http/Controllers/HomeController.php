98c3a959a81ac0a83ffb77af35a96b9be385fd51
nymeria-1 <mjflagonoy@tip.edu.ph>
2023-04-12 18:36:15 +0800

    Restrict home route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('apiauth');
+    }
+    
     public function index(){
         return view('home');
     }
